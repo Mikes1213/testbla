1cd989d8e701aaf0be03a367e7de0dcd3bbb2297
Mikes1213 <45215727+Mikes1213@users.noreply.github.com>
2018-11-23 19:41:58 +0100

    Update Fajnybot.py

--- Fajnybot.py
+++ Fajnybot.py
@@ -19,12 +19,10 @@ client.remove_command('help')
 
 async def status_task():
     while True:
-        await client.change_presence(game=discord.Game(name='for d!help'))
-        await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
-        await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
-        await asyncio.sleep(5)
+        await client.change_presence(game=discord.Game(name='Paradise RP'))
+        await asyncio.sleep(2)
+        await client.change_presence(game=discord.Game(name='Najlepszy serwer RolePlay'))
+        await asyncio.sleep(2)
         
 
 	
@@ -34,7 +32,7 @@ async def on_ready():
     print('--------')
     print('--------')
     print('Started Dark BOT')
-    print('Created by Utkarsh')
+    print('Created by David')
     client.loop.create_task(status_task())
 
 
